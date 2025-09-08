b76dbc951b480a7c786d1b3304582be540e11f9d
Suryaraj Timsina <timsinasuryaraj@gmail.com>
2025-09-08 13:15:44 +0545

    Update path

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,13 +3,14 @@ set -e
 
 echo "running install_dependencies.sh" >> /home/ubuntu/app.log
 
-# Ensure Node.js and npm are installed
 if ! command -v npm &> /dev/null
 then
   echo "Node.js not found, installing..." >> /home/ubuntu/app.log
   curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
   sudo apt-get install -y nodejs
 fi
+
+cd /home/ubuntu/app
 
 npm install --production >> /home/ubuntu/app.log 2>&1
 
