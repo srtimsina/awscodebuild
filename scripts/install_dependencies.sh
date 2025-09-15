6d3d7ca195dbc8ba43d4283e57ba1026e9f74110
Suryaraj Timsina <timsinasuryaraj@gmail.com>
2025-09-15 10:09:17 +0545

    update pkg index

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,6 +6,7 @@ echo "running install_dependencies.sh" >> /home/ubuntu/app.log
 if ! command -v npm &> /dev/null
 then
   echo "Node.js not found, installing..." >> /home/ubuntu/app.log
+  sudo apt update
   curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
   sudo apt-get install -y nodejs
 fi
