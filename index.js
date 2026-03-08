65ebb47833fb581242b6313f60b523fd3e301703
srtimsina <timsinasuryaraj@gmail.com>
2026-03-08 08:13:02 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from AWS EC2, we are deploying with CodeDeploy! App version is v2.0");
+  res.end("Hello from AWS EC2, we are deploying with CodeDeploy! App version is v2.1");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
